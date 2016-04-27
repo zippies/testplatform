5ea0d8d5a527af0adac01b33ddf8ec9253337d00
chao.chen <chenzichaocc@163.com>
2016-04-27 20:23:49 +0800

    modify

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 import os
 
 class Config:
-    DEBUG = True
+    DEBUG = False
     #SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root@localhost:3306/websnail'
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(os.path.abspath(os.path.dirname(__file__)),'data.sqlite')
     SECRET_KEY = 'what does the fox say?'
